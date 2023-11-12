c3aeb96d43bbb8c7fc8f214d05918f0dcfdbc28b
Siddharth Sunil Patil <siddharth.vdipl@gmail.com>
2023-11-12 12:45:35 -0500

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,40 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    concat_data_path: str=os.path.join('artifacts',"concat.csv")
+    
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+    
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method")
+        try:
+            train_df=pd.read_csv('Notebook\\data\\train.csv')
+            test_df=pd.read_csv('Notebook\\data\\test.csv')
+            logging.info('Read the dataset')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df=pd.concat([train_df,test_df])
+            df.to_csv(self.ingestion_config.concat_data_path,index=False,header=True)
+
+            logging.info("Ingestion of the data is completed")
+
+            return self.ingestion_config.concat_data_path
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
